8de051ba3c9ae4a741643c1a72747de6a35ff310
Ahmed Hossameldin <69988399+ahmedhossameldin96@users.noreply.github.com>
2020-12-13 18:51:53 +0300

    Add files via upload

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/catkin_ws/src
+# Build directory: /home/user/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(pkg2)
+subdirs(project_robotics_1)
